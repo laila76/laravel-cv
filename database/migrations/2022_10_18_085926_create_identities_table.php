9d46d2b430b5414e56d8da80b806060582cfeafe
laila76 <laila.elmohannii@gmail.com>
2022-10-18 11:48:12 +0200

    table created

--- database/migrations/2022_10_18_085926_create_identities_table.php
+++ database/migrations/2022_10_18_085926_create_identities_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('identities', function (Blueprint $table) {
             $table->id();
+            $table->string('url_img');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('job');
+            $table->text('description');
+            $table->integer('tel');
+            $table->string('e_mail')->unique();
+            $table->string('street');
+            $table->string('cp_city');
             $table->timestamps();
         });
     }
